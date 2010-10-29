8cbd73cf4c08b150cb04e46aee325c215e656907
Philipp Schächtele <philipp.schaechtele@googlemail.com>
2010-10-30 01:18:27 +0200

    Updating path to diem in configuration

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '/home/thib/data/workspace/diem/dmCorePlugin/lib/core/dm.php';
+require_once '/var/www/diem2/lib/vendor/diem/dmCorePlugin/lib/core/dm.php';
 dm::start();
 
 class ProjectConfiguration extends dmProjectConfiguration
